794d679023413fdf67e490b8a6693cd4167d2d30
Piyush <yadavpiyush222@gmail.com>
2023-01-18 23:17:04 +0530

    added Regex project

---
+++ REGEX Project/form.js
@@ -0,0 +1,7 @@
+username = document.querySelector("#username");
+form = document.querySelector("form");
+if (username.value == "hello") {
+  console.log("correct");
+} else {
+  console.log("incorrect");
+}
