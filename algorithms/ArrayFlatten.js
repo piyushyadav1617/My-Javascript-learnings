8f0cc06241a29b54461029ac5127244152db9419
Piyush <yadavpiyush222@gmail.com>
2023-03-29 11:25:12 +0530

    added arrayFlatten by reduce method

--- algorithms/ArrayFlatten.js
+++ algorithms/ArrayFlatten.js
@@ -10,3 +10,25 @@ function steamrollArray(arr) {
 }
 
 console.log(steamrollArray([1, [2], [3, [[4]]]]));
+
+function flatten(arr) {
+  let newArr = arr.reduce((initial, elem) => {
+    if (elem instanceof Array) { return initial.concat(flatten(elem)) }
+    else {
+      initial.push(elem)
+      return initial;
+    }
+  }, [])
+
+  return newArr
+}
+
+// const flatten = arr => arr.reduce((a, v) => {
+//   if (v instanceof Array) {
+//     return a.concat(flatten(v))
+//   } else {
+//     a.push(v);
+//     return a;
+//   }
+// }, [])
+console.log(flatten([1, [2], [3, [[4], [4, [7, [8, [10, [55]], [5]]]]]]]));
---
+++ clock/script.js
@@ -0,0 +1,12 @@
+let elem = document.createElement("p");
+elem.class = "date";
+document.body.appendChild(elem);
+
+let date = new Date();
+setInterval(() => {
+    let date = new Date();
+    elem.innerHTML = date.toLocaleTimeString();
+}, 1000)
+
+
+
