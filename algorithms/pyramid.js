cc76c71936fe1fb62c328f2862e82750c9148dec
Piyush <yadavpiyush222@gmail.com>
2023-06-10 23:01:46 +0530

    pyramid refactored

--- algorithms/pyramid.js
+++ algorithms/pyramid.js
@@ -1,12 +1,11 @@
 function pattern(rows) {
     let space = Math.floor((2 * rows - 1) / 2);
-    let n = 1;
+
     let string = "";
-    for (let i = 0; i < rows; i++) {
+    for (let i = 1; i <= rows; i++) {
         string += " ".repeat(space);
-        string += "*".repeat(i + n);
+        string += "*".repeat(2 * i - 1);
         string += '\n'
-        n++;
         space -= 1;
     }
     return string
