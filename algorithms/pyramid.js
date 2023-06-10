13ea0aea6c365caa6fbb7bf3548250707bd8b99c
Piyush <yadavpiyush222@gmail.com>
2023-06-10 23:05:23 +0530

    pyramid added comments

--- algorithms/pyramid.js
+++ algorithms/pyramid.js
@@ -1,14 +1,18 @@
 function pattern(rows) {
+    // Calculate the number of spaces for the first row
     let space = Math.floor((2 * rows - 1) / 2);
 
-    let string = "";
+    let string = ""; // Initialize an empty string to store the pattern
+
     for (let i = 1; i <= rows; i++) {
-        string += " ".repeat(space);
-        string += "*".repeat(2 * i - 1);
-        string += '\n'
-        space -= 1;
+        string += " ".repeat(space); // Add spaces before each row
+        string += "*".repeat(2 * i - 1); // Add asterisks for each row
+        string += '\n' // Add a newline character to move to the next row
+        space -= 1; // Decrease the number of spaces for the next row
     }
-    return string
+
+    return string; // Return the pattern as a string
 }
+
 
 console.log(pattern(10));
